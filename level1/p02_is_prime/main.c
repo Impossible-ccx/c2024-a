9fe3053c9b0151c9a41830c10ffe4649b4dac652
impossible.ccx@gmail.com <impossible.ccx@gmail.com>
2024-09-30 16:50:13 +0800

    试交部分作业？

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,23 @@
-#include <printf.h>
-
+#include <stdbool.h>
+#include <stdio.h>
+bool IsPrime(int input) {
+    if (input == 1) return false;
+    if (input == 2) return true;
+    for (int i = 2; i * i <= input; i++) {
+        if (input % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 int main() {
-    printf("hello world!\n");
+    int input;
+    scanf("%d", &input);
+    if (IsPrime(input)) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
     return 0;
 }
--- level1/p07_maze/main.c
+++ level1/p07_maze/main.c
@@ -1,6 +1,17 @@
-#include <printf.h>
-
+#include <stdio.h>
+#include<conio.h>
+char map[20][20];
+void fps() {
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            printf("%c", map[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main() {
-    printf("hello world!\n");
+   
+    
+    getch();
     return 0;
 }
